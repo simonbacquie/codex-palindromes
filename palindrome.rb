9cd816dde245c027fa69cd2c177e589c3764015b
SimonMac <simonblah@gmail.com>
2014-08-22 10:13:44 -0400

    Added ability to run provided example strings

--- palindrome.rb
+++ palindrome.rb
@@ -3,10 +3,22 @@ if in_string.nil?
 	puts "** You must pass a string of letters as an argument to this script."
 	exit
 end
+if in_string == "?"
+	example_num = ARGV[1].to_i
+	if !(1..5).include? example_num
+		puts "** You must pass an example number (1-5) as the second argument."
+		exit
+	end
+	example_index = example_num - 1
+	examples = ["at", "gag", "cacatat", "ttgatgggt", "cacaattcccatgggttgtggag"]
+	in_string = examples[example_index]
+end
 last_index = in_string.length - 1
 
 palindromes = []
 longest_palindrome = ""
+
+examples = ["at", "gag", "cacatat", "ttgatgggt", "cacaattcccatgggttgtggag"]
 
 (0..last_index).each do |substr_start|
 	(substr_start..last_index).each do |substr_end|
