f2346ab9716bddcf3f7a00d4bbe62bea58235521
SimonMac <simonblah@gmail.com>
2014-08-22 09:56:46 -0400

    Cleanup and validation

--- palindrome.rb
+++ palindrome.rb
@@ -1,14 +1,15 @@
-in_string = "gattacacctcc"
 in_string = ARGV[0]
+if in_string.nil?
+	puts "** You must pass a string of letters as an argument to this script."
+	exit
+end
 last_index = in_string.length - 1
 
 palindromes = []
 longest_palindrome = ""
 
 (0..last_index).each do |substr_start|
-	# puts in_string[substr_start..last_index]
 	(substr_start..last_index).each do |substr_end|
-		# puts in_string[substr_start..substr_end]
 		substr = in_string[substr_start..substr_end]
 		unless substr.length < 2
 			if substr == substr.reverse
@@ -21,7 +22,7 @@ longest_palindrome = ""
 	end
 end
 
-if longest_palindrome.length
+if longest_palindrome.length > 0
 	puts "** #{palindromes.length} palindromes were found:"
 	palindromes.each do |pal|
 		puts pal
